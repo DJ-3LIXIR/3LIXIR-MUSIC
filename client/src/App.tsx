2145db09154387de769d0014d66b2a168e98b5f2
Jared Frazier <3lixirbeats@gmail.com>
2026-03-13 17:29:37 -0700

    ADDED APOLLO.TSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,6 +31,7 @@ import Store from "@/pages/Store";
 import VST from "@/pages/VST";
 import LoaderPage from "@/pages/3lixirloader";
 import ARK from "@/pages/ARK";
+import Apollo from "@/pages/apollo";
 
 // Contract Pages
 import Terms from "@/components/Shop/Contract/Terms";
@@ -64,6 +65,7 @@ function Router() {
       <Route path="/vst" component={VST} />
       <Route path="/loader" component={LoaderPage} />
       <Route path="/ark" component={ARK} />
+      <Route path="/apollo" component={Apollo} />
       <Route path="/stripe-success" component={StripeSuccess} />
       <Route path="/cancel" component={Cancel} />
       <Route path="/downloads" component={Downloads} />
