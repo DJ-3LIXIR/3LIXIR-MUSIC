96d6500711198a987543b4018fea94e406d90013
Jared Frazier <3lixirbeats@gmail.com>
2026-05-02 16:05:48 -0700

    Fixed the 3lixirloader download page sub nav bar

--- client/src/pages/3lixirloader.tsx
+++ client/src/pages/3lixirloader.tsx
@@ -165,7 +165,7 @@ export default function LoaderPage() {
         {[
           { label: "Beats", href: "/beats" },
           { label: "VST Plugins", href: "/vst" },
-          { label: "Merchandise", href: "/shop" },
+          { label: "Merchandise", href: "/merchandise" },
           { label: "Plugin Installer", href: "/loader" },
         ].map((item, index) => (
           <Link key={item.href + index} href={item.href}>
